3130255319f5f8d3275d682b43605a40b9fcfda3
L0laapk3 <L0laapk3@gmail.com>
2020-11-13 21:41:52 +0100

    fix const

--- CardBoard.h
+++ CardBoard.h
@@ -9,7 +9,7 @@ typedef std::array<const CardBoard, 5> GameCards;
 
 class CardBoard : public Card {
 public:
-	CardBoard(CardBoard& card);
+	CardBoard(const CardBoard& card);
 	CardBoard(const Card* card);
 	std::array<std::array<uint32_t, 25>, 2> moveBoard;
 
